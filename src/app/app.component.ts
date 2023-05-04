096c8d3928bcfbae9905fbbc656cbdb590bc34c5
Ricardo <richy.2603@gmail.com>
2023-05-04 01:21:03 -0500

    Iniciamos la implementación del consumo de la API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prueba-filtro-opi';
+
+  http = inject(HttpClient);
+
+  // Iniciamos el componente para
+  ngOnInit() {
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe(data => {
+      console.log(data);
+    });
+  }
 }
