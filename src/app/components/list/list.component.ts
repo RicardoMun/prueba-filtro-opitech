6f5f8f44a35fd9dd1d59287202b78249d24a549c
Ricardo <richy.2603@gmail.com>
2023-05-03 21:34:44 -0500

    Queda agregado en link a la hoja de estilos principal

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+
+  // Array of users for the list
+  @Input() Users: string = '';
 
 }
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,3 +1,17 @@
-interface User {
+let status = ['landed', 'operating', 'delayed', 'cancelled'];
+
+export class User {
+
+  name: string;
+  email: string;
+  status: string;
+  gender: string;
+
+  constructor(name:string, email:string, status:string, gender:string){
+    this.name = name;
+    this.email = email;
+    this.status = status[Math.floor(Math.random() * status.length)];
+    this.gender = gender;
+  }
 
 }
